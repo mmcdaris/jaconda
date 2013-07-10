2a15d7bf91f1e451ca49b6810dc6ad4b4aa11e0e
mmcdaris <morganmcdaris@yahoo.com>
2013-07-09 17:45:23 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jaconda::Application.config.secret_token = 'b9d5762747697bc9da1d3a5a1ffd363c60b0f6975dc5179787539a2b901720c7319b910d8869cb8d3a8b763a6d4d3b613ac4f5f69d89ae4bce3723687bdf7ba9'
